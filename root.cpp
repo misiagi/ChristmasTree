08a2a1526e26cf4586f1e91e388e5b7249af26f0
misiagi <misiagi@o2.pl>
2014-01-29 00:40:46 +0100

    Create root.cpp

---
+++ root.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "root.h"
+
+
+root::root(void)
+{
+}
+
+
+root::~root(void)
+{
+}
+
+void root::setwidth(int _width)
+{
+	width=_width;
+}
+
+int root::getwidth()
+{
+	return width;
+};
